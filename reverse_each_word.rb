4689d19aa314e26ce68cd314366ed9e773b247f1
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-18 23:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  split_string = string.split(" ") #=> splits string into seperate parts
+   = string.split(" ") #=> splits string into seperate parts
   string.split.each()
   binding.pry
 end
