461aed9198f5db24038798be7504ee8354bada29
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-18 23:50:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  sentence1 = string.split(" ") #=> splits string into seperate parts and stores it as an array
+# string.split(" ") #=> splits string into seperate parts and stores it as an array
   sentence1.reverse_each { |sentence1| str += "#{sentece1} " } #=> enumerator each string
 # string.split.each(|sentence| sentence ) #=> enumerator each string
   string.reverse_each
