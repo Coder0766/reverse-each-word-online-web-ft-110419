d228ff29249dea2579a741e4711713e4fd01394a
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-18 23:52:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
-
+def reverse_each_word(string)
+    string.split.each_reverse
+    binding.pry
+end
 
 
 
@@ -9,11 +12,11 @@ require 'pry'
 
 
 # def reverse_each_word(string)
-# # string.split(" ") #=> splits string into seperate parts and stores it as an array
+#   string.split(" ") #=> splits string into seperate parts and stores it as an array
 #   string.split.each do |sentence1) #=> enumerator each string
 #   sentence1.reverse_each { |sentence1| str += "#{sentece1} " } #=> enumerator each string
 #   string.reverse_each
-# # binding.pry
+#   binding.pry
 # end
 
 
